7ecf36bd6ff31889a97c015860a82f60ed95df6a
Kecar2 <kendrycarvajalsegura@gmail.com>
2022-05-08 10:19:34 +0200

    añadido tabla de trabajo destacado

--- src/main/java/com/hiring/empleos/controller/HomeController.java
+++ src/main/java/com/hiring/empleos/controller/HomeController.java
@@ -89,6 +89,7 @@ public class HomeController {
 			vacante1.setDescripcion("¡Estamos a cargo del feed que cada usuario mira cada vez que inicia sesión en nuestra aplicación! Estamos buscando un ingeniero de back-end para unirse y ayudarnos a construir una experiencia de usuario simple y relevante al capacitar a nuestros usuarios para que participen en sus transacciones, mientras impulsamos una cultura de tutoría e intercambio de conocimientos.");
 			vacante1.setFecha(sdf.parse("05-05-2022"));
 			vacante1.setSalario(5000.0);
+			vacante1.setDestacado(1);
 			
 			// Creamos la oferta de Trabajo 2.
 			Vacante vacante2 = new Vacante();
@@ -97,6 +98,8 @@ public class HomeController {
 			vacante2.setDescripcion("Estamos buscando nuevos miembros para unirse a nuestros nuevos equipos, que se centrarán en crear nuevos productos para ofrecer a los fanáticos del deporte una experiencia más estimulante.");
 			vacante2.setFecha(sdf.parse("05-05-2022"));
 			vacante2.setSalario(2000.0);
+			vacante2.setDestacado(0);
+
 			
 			// Creamos la oferta de Trabajo 3.
 			Vacante vacante3 = new Vacante();
@@ -105,6 +108,8 @@ public class HomeController {
 			vacante3.setDescripcion("Como desarrollador en Sagins, diseñarás, desarrollarás y entregarás productos o sistemas de alta calidad que superen las expectativas de nuestros clientes. Te unirás a un equipo de desarrollo motivado, talentoso y colaborativo que trabaja con equipos en todo el mundo compartiendo su conocimiento y experiencia.");
 			vacante3.setFecha(sdf.parse("05-05-2022"));
 			vacante3.setSalario(7000.0);
+			vacante3.setDestacado(0);
+
 			
 			// Creamos la oferta de Trabajo 4.
 			Vacante vacante4 = new Vacante();
@@ -113,6 +118,8 @@ public class HomeController {
 			vacante4.setDescripcion("En EY tendrás la oportunidad de construir una experiencia única. Dispondrás de la última tecnología, estarás rodeado de equipos de alto rendimiento y contarás con el entorno global y la cultura inclusiva que necesitas para descubrir tu potencial. A través de nuestros cursos de formación y mentoring, podrás desarrollar las habilidades necesarias para crecer, a día de hoy y en el futuro; mientras construyes una red de contactos, mentores y líderes que te acompañaran en esta experiencia en EY.");
 			vacante4.setFecha(sdf.parse("05-05-2022"));
 			vacante4.setSalario(2500.0);
+			vacante4.setDestacado(1);
+
 			
 			/**
 			 * Agregamos los 4 objetos de tipo Vacante a la lista...
